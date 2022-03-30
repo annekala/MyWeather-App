5839cf0aa7c222017c5c60064c45b7f5ffcf74a2
Anne Kala <annekala1127@gmail.com>
2022-03-29 23:09:59 -0700

    Forecast with API

--- script/json.js
+++ script/json.js
@@ -20,7 +20,7 @@ function formatDate(date) {
   return `${days[day]} ${hour}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -50,6 +50,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "24ac2360a2fa6e18066acab43e4b5d52";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayWeather(response) {
   console.log(response.data);
   document.querySelector("#citty").innerHTML = response.data.name;
@@ -73,6 +78,8 @@ function displayWeather(response) {
     (document.querySelector("#icon").innerHTML = "alt"),
     response.data.weather[0].main
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchFor(city) {
@@ -135,4 +142,3 @@ let fahrenheitlink = document.querySelector("#Fahrenheit-link");
 fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
 let iconElement = document.querySelector("#icon");
 searchFor("New York");
-displayForecast();
